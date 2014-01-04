c61b245d4a583b9e97a195e417c4fba5b6441312
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-01-04 11:40:30 -0200

    ok

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -3,13 +3,31 @@ class RedirectController < ApplicationController
     @url = params[:redirect_to]
     @trace = Trace.new
     @trace.data = "igor"
-    @trace.save
+
+    
+    #@trace.save
+    
+    respond_to do |format|
+      if @trace.save
+        format.html { redirect_to @trace, notice: 'Trace was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @trace }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @trace.errors, status: :unprocessable_entity }
+      end
+    end
+
+    
+    
     @data = params.to_s
 
     logger.debug "AQUI"
     logger.debug @data
     logger.debug Trace.where(:data => @data).empty?
     logger.debug "--"
+
+
+    
 
     #if Trace.where(:data => @data).empty?
     #@trace.data = @data
