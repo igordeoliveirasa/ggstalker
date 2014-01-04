09cd02d5b79adbd4577284ff53f4045dcc19de5e
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-01-04 11:05:59 -0200

    gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ggstalker::Application.config.secret_key_base = 'd851b27cb192175eaad24fcf68db702a4c8fe715acefefaec17ecb313ff17d5d8db15405de45115232431bb18e679cd2a191ff2c478c3d44d81260b7a8ad60e8'
